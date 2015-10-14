730a61aef662d4cadfb998cd64e6ffe3a2dd9f81
Alejandro <AlexMJHS@gmail.com>
2015-10-14 08:37:58 -0600

    Add Saving State to repo.

---
+++ app/src/main/java/activity/ctec/savingstateapplication/controller/FrontActivity.java
@@ -0,0 +1,15 @@
+package activity.ctec.savingstateapplication.controller;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FrontActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_front);
+    }
+}
---
+++ app/src/main/java/ctec/savingstateapplication/model/AndroidSaveState.java
@@ -0,0 +1,50 @@
+package ctec.savingstateapplication.model;
+
+import android.app.Application;
+
+/**
+ * Created by amag6880 on 10/14/15.
+ */
+public class AndroidSaveState extends Application
+{
+    private String userName;
+    private boolean isTired;
+    private int age;
+
+    public AndroidSaveState()
+    {
+        this.userName = "default";
+        this.isTired = fasle;
+        this.age = -0;
+    }
+
+    public String getUserName()
+    {
+        return userName;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+
+    public boolean getIsTired()
+    {
+        return isTired;
+    }
+
+    public void setUserName(String userName)
+    {
+        this.userName = userName;
+    }
+
+    public void setIsTired(boolean isTired)
+    {
+        this.isTired = isTired;
+    }
+
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
+}
